6355d376f67b9f380fbc38e7103d0e356bdf8791
wuxiaoran <wxrbw@qq.com>
2018-10-06 22:50:31 +0800

    feat($config): 设置husky

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 tar -zcvf ../web-admin.tar.gz --exclude ./node_modules .
 scp -r ../web-admin.tar.gz ubuntu@123.206.90.112:~/sites
-ssh ubuntu@123.206.90.112 "cd ~/sites; rm -rf ./web-admin-test; mkdir web-admin-test; tar -zxvf web-admin.tar.gz ./web-admin-test; cd ./web-admin-test; pwd; yarn install; yarn run build"
+ssh ubuntu@123.206.90.112 "cd ~/sites; rm -rf ./web-admin-test;
+ mkdir web-admin-test; cp ./web-admin.tar.gz ./web-admin-test/;
+  cd ./web-admin-test; tar -zxvf web-admin.tar.gz;
+  pwd; yarn install; yarn run build"
